d0b6f734d8d747357727f75d675586afb2ad3d16
Gil Gam <gilbertoandreattamaia@gmail.com>
2021-12-19 11:55:34 -0300

    calcula media E multiplos de 10

---
+++ calcula_media/Program.cs
@@ -0,0 +1,44 @@
+﻿//Crie um algoritmo que leia as notas obtidas por um aluno e calcule a média entre elas. 
+//O programa deve parar de ler notas e exibir a média quando digitada a nota 11.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace passo_5_exercicio_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<double> notas = new List<double>();
+            Console.WriteLine("Digite as notas \n Digite 11 para calcular a média: ");
+            double nota = 0;
+            while (true)
+            {
+                nota = Double.Parse(Console.ReadLine());
+                notas.Add(nota);
+                if (nota == 11)
+                {
+                    break;
+                }
+            }
+
+            try
+            {
+                double media = (notas.Sum() - 11) / (notas.Count - 1);
+
+                Console.WriteLine("\n A média é: {0}", media);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+    }
+}
---
+++ multiplos_10/Program.cs
@@ -0,0 +1,23 @@
+﻿//Faça um algoritmo que conte de 1 a 100 e a cada múltiplo de 10 emita uma mensagem: 
+//“Múltiplo de 10”
+
+
+using System;
+
+namespace passo_5_exercicio_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            for (int x = 1; x <= 100; x++)
+            {
+                if (x % 10 == 0)
+                {
+                    Console.WriteLine("Múltiplo de 10: {0}", x);
+                }
+            }
+        }
+    }
+}
