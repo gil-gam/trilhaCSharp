1122c95c577e3ae198129f94fe267cdf8d5d6de8
Gil Gam <gilbertoandreattamaia@gmail.com>
2021-12-25 03:00:29 -0300

    25/12/2021

---
+++ fatorial/Program.cs
@@ -0,0 +1,32 @@
+﻿///<sumary>
+///Faça um algoritmo que lê um valor N inteiro e positivo e que calcula e escreve o fatorial de N (N!).
+/// </sumary>
+
+using System;
+using System.Linq;
+
+
+namespace fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número inteiro e positivo: ");
+            int numero = Int32.Parse(Console.ReadLine());
+            int i = 0;
+            int fatorial = numero;  
+            
+            for (i = numero - 1; i >= 1; i--)
+            {
+                if (numero <= 0)
+                    continue;
+
+                numero = numero * i;                
+            }
+
+            Console.WriteLine("O fatorial de " + fatorial + " é: " + numero);
+
+        }
+    }
+}
---
+++ tabuada/Program.cs
@@ -0,0 +1,28 @@
+﻿/// <summary> Faça a tabuada de um número e apresente o resultado de acordo com o modelo a seguir, 
+/// sabendo que o multiplicando deverá ser fornecido pelo usuário (n) e a quantidade de iterações também (i).
+/// </summary>
+/// 
+using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o número para a tabuada: ");
+            int numero = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Informe quantas vezes vai multiplicar: ");
+            int vezes = Int32.Parse(Console.ReadLine());
+            int multiplicador = numero;
+            int i = 0;
+
+            for (i = 1; i <= vezes; i++)
+            {
+                multiplicador = numero * i;
+                Console.WriteLine(numero + " X " + i + " = " + multiplicador);
+            }
+
+        }
+    }
+}
