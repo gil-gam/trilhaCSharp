cc45bc32ec795d176de71cec0c4a04f39083153c
Gil Gam <gilbertoandreattamaia@gmail.com>
2021-12-26 01:44:59 -0300

    26/12/2021

---
+++ vetorPrimosEPosicao/Program.cs
@@ -0,0 +1,49 @@
+﻿/// <sumary>Faça um algoritmo em pseudocódigo e fluxograma, que carregue um vetor de nove elementos
+/// numéricos inteiros, calcule e mostre os números primos e suas respectivas posições dentro do vetor
+/// (considere um vetor zero-based).
+/// </sumary>
+
+using System;
+using System.Linq;
+
+namespace vetorPrimosEPosicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[9];
+            int y = 0;
+            int posicao = 0;
+
+            Console.WriteLine("Digite 9 números, seguidos de ENTER");
+            for (int i = 0; i < 9; i++)
+            {
+                vetor[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            foreach (int x in vetor)
+            {
+                for (y = 2; y <= vetor.Length; y++)
+                {
+                    posicao += 1;
+                    if (x == 1)
+                    {
+                        break;
+                    }
+                    else if (x % y == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("O número {0}", + x + " é primo! Posição: " + posicao);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
