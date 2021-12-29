07886c791e577324286c6909fb353274b49320ec
Gil Gam <gilbertoandreattamaia@gmail.com>
2021-12-29 19:48:41 -0300

    29/12/2021 - mvc

---
+++ MVC/Lab01_ApresentacaoMVC/Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab01_ApresentacaoMVC.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public bool Assistido { get; set; }
+    }
+}
